e32a36b5b81ef8044c1560fbfa5e5a7b690515f9
PBM42 <PBM764927@gmail.com>
2017-12-13 16:43:10 +0100

    fix(main.flow): resolveConflicts unknow IQuery

--- src/data-flows/main.flow.ts
+++ src/data-flows/main.flow.ts
@@ -1,6 +1,8 @@
+import { IQuery } from '@autoschedule/queries-fn';
+
 import { IContext } from '../data-structures/context.interface';
 import { IDependency } from '../data-structures/dependency.interface';
 
-export const resolveConflicts = (dependency: IDependency) => (context: IContext) => {
+export const resolveConflicts = (dependency: IDependency) => (context: IContext): Promise<ReadonlyArray<IQuery>> => {
   return Promise.resolve(context.queries);
 };
