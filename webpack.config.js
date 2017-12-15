712855a37ca8a3c7e141403ffa19aeec68d28976
PBM42 <pbm764927@gmail.com>
2017-12-15 08:19:25 +0100

    ci: add codeclimate test reporter

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,37 @@ const webpack = require('webpack');
 const path = require('path');
 const nodeEnv = process.env.NODE_ENV || 'development';
 const isProd = nodeEnv === 'production';
+const libname = 'conflict-resolver';
 
 const config = {
-	entry: './src/index.ts',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.bundle.js',
-	},
-	resolve: {
-		extensions: ['.js', '.ts', '.json'],
-	},
-	devtool: isProd ? 'hidden-source-map' : 'cheap-module-eval-source-map',
-	plugins: [
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: JSON.stringify(nodeEnv),
-			},
-		}),
-		new webpack.optimize.ModuleConcatenationPlugin(),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				loader: 'ts-loader',
-				options: { onlyCompileBundledFiles: true, compilerOptions: { outDir: '.' } },
-			},
-		],
-	},
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
+    library: libname,
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.json'],
+  },
+  devtool: isProd ? 'hidden-source-map' : 'cheap-module-eval-source-map',
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(nodeEnv),
+      },
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: { onlyCompileBundledFiles: true, compilerOptions: { outDir: '.' } },
+      },
+    ],
+  },
 };
 
 module.exports = config;
